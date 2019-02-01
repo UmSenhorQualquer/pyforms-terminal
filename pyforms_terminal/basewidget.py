4873957eaaba2fe08ee81abb3de7dac4f4785241
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-02-01 16:44:18 +0000

    Fix imports

--- pyforms_terminal/basewidget.py
+++ pyforms_terminal/basewidget.py
@@ -13,13 +13,14 @@ from datetime import datetime, timedelta
 import argparse, uuid, os, shutil, time, sys, subprocess
 import simplejson as json
 
+import logging
 
+logger=logging.getLogger(__file__)
 
 try:
     import requests
 except Exception as e:
-    import logging
-    logger=logging.getLogger(__file__)
+
     logger.warning("No requests lib")
     logger.error(e, exc_info=True)
 
@@ -181,7 +182,6 @@ class BaseWidget(object):
     def end_progress(self):
         self._processing_count = self._total_processing_count
         self.update_progress()
-
 
 
     def __savePID(self, pid):
